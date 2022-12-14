23569b52a8a1362b08d3226eb828eb9eaedcb94b
Larissa Silva <larissalarissasilva62@gmail.com>
2022-12-14 18:34:11 -0300

    Implementando uma função getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const gere = data.employees.some((employee) => employee.managers.includes(id));
+  return gere;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  const gere = data.employees.filter((employee) => employee.managers.includes(managerId));
+  const result = [];
+  const idMana = isManager(managerId);
+
+  if (idMana === true) {
+    gere.forEach((person) => {
+      const { firstName, lastName } = person;
+      result.push(`${firstName} ${lastName}`);
+    });
+    return result;
+  }
+
+  if (idMana === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 };
 
 module.exports = { isManager, getRelatedEmployees };
