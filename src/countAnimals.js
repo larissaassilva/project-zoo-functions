f4ae2d9e1c33bafc6417755d6e0af9861eef8724
Larissa Silva <larissalarissasilva62@gmail.com>
2022-12-14 22:46:19 -0300

    Obtendo 80% de cobertura de testes na função handlerElephants

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  // const {species} = data;
+  // const result = {};
+  // if (animal === undefined) {
+  //   const vazio = species.forEach((specie) => {
+  //     const { name, residents } = specie;
+  //     let
+  //     let novo = {name, residents.length};
+  //     Object.assign(result, novo);
+  //   });
+  //   return result;
+  // }
 };
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,37 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Ao chamar a função com o parâmetro `count`, retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Ao chamar a função com o parâmetro `names`, rretorna um array com a relação dos nomes de todos os elefantes', () => {
+    const actual = handlerElephants('names');
+    const actual2 = handlerElephants('Names');
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(actual).toEqual(expected);
+  });
+
+  it('Ao chamar a função com o parâmetro `averageAge`, retorna a média de idade dos elefantes', () => {
+    const actual = handlerElephants('averageAge');
+    const expected = 10.5;
+    expect(actual).toEqual(expected);
+  });
+
+  it('Ao chamar a função com o parâmetro `location`, retorna a localização dos elefantes dentro do Zoológico', () => {
+    const actual = handlerElephants('location');
+    const expected = 'NW';
+    expect(actual).toEqual(expected);
+  });
+
+  it('Ao chamar a função com o parâmetro `popularity`, retorna a popularidade dos elefantes', () => {
+    const actual = handlerElephants('popularity');
+    const expected = 5;
+    expect(actual).toEqual(expected);
+  });
+
+  it('Ao chamar a função com o parâmetro `availability`, retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    const actual = handlerElephants('availability');
+    const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+    expect(actual).toEqual(expected);
+  });
+});
