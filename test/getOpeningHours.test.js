ff1b67e15be102cb08347375bed13d91f55c53d4
Larissa Silva <larissalarissasilva62@gmail.com>
2022-12-15 23:44:23 -0300

    Obtendo 95% de cobertura de testes na função getOpeningHours

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -14,19 +14,31 @@ describe('Testes da função getOpeningHours', () => {
     };
     expect(actual).toEqual(expected);
   });
-  it('Para os argumentos Monday e 09:00-AM deve retornar a string `The zoo is closed` (Já que o Zoo está sempre fechado na segunda);', () => {
+  it('Ao passar os argumentos Monday e 09:00-AM deve retornar a string `The zoo is closed` (Já que o Zoo está sempre fechado na segunda);', () => {
     const actual = getOpeningHours('Monday', '09:00-AM');
     const expected = 'The zoo is closed';
     expect(actual).toEqual(expected);
   });
-  it('Para os argumentos Tuesday e 09:00-AM deve retornar a string `The zoo is open`', () => {
+  it('Ao passar os argumentos Tuesday e 09:00-AM deve retornar a string `The zoo is open`', () => {
     const actual = getOpeningHours('Tuesday', '09:00-AM');
     const expected = 'The zoo is open';
     expect(actual).toEqual(expected);
   });
-  it('Para os argumentos Wednesday e 09:00-PM deve retornar a string `The zoo is closed`', () => {
+  it('Ao passar os argumentos Wednesday e 09:00-PM deve retornar a string `The zoo is closed`', () => {
     const actual = getOpeningHours('Wednesday', '09:00-PM');
     const expected = 'The zoo is closed';
     expect(actual).toEqual(expected);
+  });
+  it('Ao passar os argumentos Thu e 09:00-AM deve lançar uma exceção com a mensagem: `The day must be valid. Example: Monday`', () => {
+    expect(() => getOpeningHours('Thu', '09:00-AM')).toThrow(/^The day must be valid. Example: Monday$/);
+  });
+  it('Ao passar os argumentos Friday e 09:00-ZM deve lançar uma exceção com a mensagem: The abbreviation must be \'AM\' or \'PM\'', () => {
+    expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrow(/^The abbreviation must be \'AM\' or \'PM\'$/);
+  });
+  it('Ao passar os argumentos Saturday e C9:00-AM deve lançar uma exceção com a mensagem: The hour should represent a number', () => {
+    expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow(/^The hour should represent a number$/);
+  });
+  it('Ao passar os argumentos Sunday e 09:c0-AM deve lançar uma exceção com a mensagem: The minutes should represent a number', () => {
+    expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow(/^The minutes should represent a number$/);
   });
 });
