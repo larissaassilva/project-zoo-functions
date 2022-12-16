1451ae9f747ddf46e084b554206349117cd3543d
Larissa Silva <larissalarissasilva62@gmail.com>
2022-12-15 23:01:49 -0300

    Obtendo 85% de cobertura de testes na função getOpeningHours

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,25 @@
+/* eslint-disable no-undef */
 const data = require('../data/zoo_data');
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
+const getEmployeesCoverage = (objeto) => {
+  // return Object.values(objeto);
+  // if (objeto === undefined) {
+  //   return 'não tem paramentro';
+  // }
+  // if ((objeto.name) === undefined) {
+  //   const lName = objeto.lastName;
+  //   return lName;
+  // }
+  // if ((objeto.name) !== undefined) {
+  //   const nameP = objeto.name;
+  //   const nameO = data.species.filter((specie) => specie.lastName === nameP);
+  //   return nameP;
+  // }
+  // const eid = objeto.id;
+  // if (eid !== undefined) {
+  //   return 'é um id';
+  // }
 };
+// console.log(getEmployeesCoverage({id: 'larissa'}));
 
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  // const a = id;
+  // id.forEach((element) => {
+  //   const person = data.species.filter((specie) => specie.name === id);
+  //   return person;
+  // });
 };
+// const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+// const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+// const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
 
+// const employees = [
+//   'c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1',
+//   burlId,
+//   olaId,
+//   '56d43ba3-a5a7-40f6-8dd7-cbb05082383f',
+//   stephanieId,
+//   '4b40a139-d4dc-4f09-822d-ec25e819a5ad',
+//   'c1f50212-35a6-4ecd-8223-f835538526c2',
+//   'b0dc644a-5335-489b-8a2c-4e086c7819a2',
+// ];
+// console.log(getOldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = getOldestFromFirstSpecies;
